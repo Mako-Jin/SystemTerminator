e9284e985511b0c7d5b0c2c498147d6b9c5d63f8
w13259720691 <846288535@qq.com>
2026-04-21 18:47:52 +0800

    实现功能：     1、处理基础设施层里面的自定义异常和国际化消息，修改日志等等

---
+++ sts-common/sts-common-infrastructure/src/main/java/com/yaocode/sts/common/infrastructure/exception/InfraException.java
@@ -0,0 +1,11 @@
+package com.yaocode.sts.common.infrastructure.exception;
+
+import com.yaocode.sts.common.basic.exception.BusinessException;
+
+/**
+ * 基础设施层公共异常
+ * @author: Jin-LiangBo
+ * @date: 2026年04月20日 19:24
+ */
+public class InfraException extends BusinessException {
+}
