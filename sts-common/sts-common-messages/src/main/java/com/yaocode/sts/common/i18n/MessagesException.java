2d326cdd819c7035ea439f9f56298fe5d206e1ca
w13259720691 <846288535@qq.com>
2026-04-21 18:44:59 +0800

    实现功能：     1、处理common-messages里面的自定义异常和国际化消息，修改日志等等

---
+++ sts-common/sts-common-messages/src/main/java/com/yaocode/sts/common/i18n/MessagesException.java
@@ -0,0 +1,11 @@
+package com.yaocode.sts.common.i18n;
+
+import com.yaocode.sts.common.basic.exception.BusinessException;
+
+/**
+ * 消息异常
+ * @author: Jin-LiangBo
+ * @date: 2026年04月20日 19:24
+ */
+public class MessagesException extends BusinessException {
+}
---
+++ sts-common/sts-common-messages/src/main/java/com/yaocode/sts/common/i18n/constants/I18nKeyConstants.java
@@ -0,0 +1,11 @@
+package com.yaocode.sts.common.i18n.constants;
+
+import com.yaocode.sts.common.basic.constants.BasicI18nKeyConstants;
+
+/**
+ * 国际化消息key
+ * @author: Jin-LiangBo
+ * @date: 2026年04月21日 15:13
+ */
+public interface I18nKeyConstants extends BasicI18nKeyConstants {
+}
