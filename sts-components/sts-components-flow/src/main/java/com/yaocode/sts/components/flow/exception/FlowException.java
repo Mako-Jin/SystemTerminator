82fc26cabaf43b89565ee8396b777ccf765e5e5e
846288853 <846288535@qq.com>
2026-06-11 23:20:01 +0800

    工作流组件一些基础类定义

---
+++ sts-components/sts-components-flow/src/main/java/com/yaocode/sts/components/flow/exception/FlowException.java
@@ -0,0 +1,10 @@
+package com.yaocode.sts.components.flow.exception;
+
+import com.yaocode.sts.common.basic.exception.BusinessException;
+
+
+/**
+ * 工作流异常
+ */
+public class FlowException extends BusinessException {
+}
---
+++ sts-components/sts-components-flow/src/main/java/com/yaocode/sts/components/flow/exception/InvalidFlowDefinitionException.java
@@ -0,0 +1,7 @@
+package com.yaocode.sts.components.flow.exception;
+
+/**
+ * 无效的流程定义异常
+ */
+public class InvalidFlowDefinitionException extends FlowException {
+}
